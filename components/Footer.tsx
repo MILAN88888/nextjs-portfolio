098c1a42dc2c26a28e380539cdc72644a2cee4dc
MILAN88888 <chaudharymilan996@gmail.com>
2023-08-26 08:59:15 +0545

    Add - Initial setup

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+	<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+		Footer
+	</div>
+  )
+}
