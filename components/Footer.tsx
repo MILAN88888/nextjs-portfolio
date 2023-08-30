839b6b3b803b06dddc109fcd3eca406fd4053e65
MILAN88888 <chaudharymilan996@gmail.com>
2023-08-30 08:40:23 +0545

    Modified - Header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-	<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-		Footer
+	<div className=' z-10 max-w-5xl w-full items-center footer-container'>
+	<hr className='mb-10 lg:max-w-5xl lg:w-full' />
+	<div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+	<p>Copyright &copy; 2023 by @milan</p>
+		<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-cente dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+
+		</div>
+  	</div>
 	</div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,10 @@ export const Header = () => {
   return (
 	<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 		<p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:p-4 lg:dark:bg-zinc-800/30">
-	 		Milan&nbsp;
+	 		<strong><span className='font-bold text-4xl title'>@Milan</span></strong>&nbsp;
 		</p>
+		<div className="bottom-0 left-0 flex  w-full items-end justify-center dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 		<ThemeSwitcher />
-		<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-			By @i-am-milan
 		</div>
   	</div>
   )
