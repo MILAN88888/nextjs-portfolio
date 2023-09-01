d65fdb5de059a55e03532153aea5c0b9db4fad15
MILAN88888 <chaudharymilan996@gmail.com>
2023-09-01 20:30:51 +0545

    Added - Menu options constant

---
+++ constants/MenuOptions.tsx
@@ -0,0 +1,5 @@
+export const MENU_OPTIONS = [
+	{ id: "about", name: "About", url: "/about" },
+	{ id: "projects", name: "Projects", url: "/projects" },
+	{ id: "tech", name: "Tech", url: "/tech" }
+];
--- constants/index.tsx
+++ constants/index.tsx
@@ -1,1 +1,2 @@
 export { SOCIAL_MEDIA } from './SocialMedia';
+export { MENU_OPTIONS } from './MenuOptions';
