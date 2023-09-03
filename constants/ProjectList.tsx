7f4191f9aa81d7fdb86709c9065d05b18e52b680
MILAN88888 <chaudharymilan996@gmail.com>
2023-09-03 14:15:01 +0545

    Enhance - Project-page

---
+++ constants/ProjectList.tsx
@@ -0,0 +1,8 @@
+export const PROJECT_LIST = [
+	{ id: "1", description: "@Milan Portfolio using Next.js", images: "/images/project/nextjsportfolio.png",liveUrl:"https://github.com/MILAN88888/nextjs-portfolio", repoUrl:"https://github.com/MILAN88888/nextjs-portfolio",stack:['Next', 'TypeScript','HTML','Tailwind', 'CSS', ], title:'Next Portfolio' },
+	{ id: "2", description: "BookExchange", images: "/images/project/bookexchange.png",liveUrl:"", repoUrl:"https://github.com/MILAN88888/Laravel-BookXchange",stack:['Laravel 8'], title:'BookExchange' },
+	{ id: "3", description: "Milan Portfolio using React", images: "/images/project/reactportfolio.png",liveUrl:"https://www.milankumarchaudhary.com.np/", repoUrl:"https://github.com/MILAN88888/techmkrr.github.io",stack:['React','HTML','CSS'], title:'React Portfolio' },
+	{ id: "4", description: "Employee Management System", images: "/images/project/ems.png",liveUrl:"", repoUrl:"https://github.com/MILAN88888/EMS",stack:['HTML','CSS','PHP','jQuery-Ajax', 'MYSQL'], title:'Employee Management System' },
+	{ id: "4", description: "Quotes, Alarm and Js Calculator", images: "/images/project/calculator.png",liveUrl:"https://milan88888.github.io/Js_calculator/index.html", repoUrl:"https://github.com/MILAN88888/Js_calculator",stack:['HTML','CSS', 'JavaScript'], title:'Quotes, Alarm and Js Calculator' },
+	{ id: "4", description: "Library Management System", images: "/images/project/libraryms.png",liveUrl:"", repoUrl:"",stack:['HTML','CSS','PHP', 'MYSQL'], title:'Library Management System' },
+];
