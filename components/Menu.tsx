ddd45d778b60cb8ad49ddd6eb192de7c22d696a2
MILAN88888 <chaudharymilan996@gmail.com>
2023-09-03 09:15:25 +0545

    Tweak - Active menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { MENU_OPTIONS } from '@/constants'
-import Link from 'next/link'
+"use client";
+import React, { useState } from "react";
+import { MENU_OPTIONS } from "@/constants";
+import Link from "next/link";
 
-export const Menu = () => {
+export const Menu = ({activeMenuItem, setActiveMenuItem}:any) => {
+
+  const handleMenuItemClick = (item: any) => {
+    setActiveMenuItem(item.id);
+  };
   return (
-	<ul className="menu-container flex items-center gap-5">
+    <ul className="menu-container flex items-center gap-5">
       {MENU_OPTIONS.map((item) => (
         <li key={item.id}>
           <Link
             href={item.url}
             aria-label={item.name}
             title={item.name}
-            className="text-2xl"
+            className={`text-2xl ${
+              activeMenuItem == item.id
+                ? "text-teal-500 font-semibold"
+                : ""
+            }`}
+            onClick={() => handleMenuItemClick(item)}
           >
             {item.name}
           </Link>
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
